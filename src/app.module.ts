fed1645161aab1c657f3148c4376fa136540f43a
Nirav Goswami <niravgoswami12@gmail.com>
2023-03-23 18:59:24 -0400

    deepscan fixes :bug:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
 import { AppController } from './app.controller';
 import { CoreModule } from './core/core.module';
 import { ExceptionsFilter } from './core/filter/exceptions.filter';
@@ -10,7 +9,7 @@ import { TransformInterceptor } from './core/interceptor/transform.interceptor';
 import { environments } from './environments/environments';
 import { FeatureModule } from './feature/feature.module';
 
-mongoose.set('debug', true);
+// mongoose.set('debug', true);
 @Module({
   imports: [
     CoreModule,
